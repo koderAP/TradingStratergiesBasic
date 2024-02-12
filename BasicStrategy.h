e39eb2ae4a6308f3f7cd4bd049cb826f331bdc97
ambarish1729 <77337286+ambarish1729@users.noreply.github.com>
2024-02-12 17:03:01 +0530

    Update BasicStrategy.h

--- BasicStrategy.h
+++ BasicStrategy.h
@@ -1,18 +1,17 @@
-void basicStrategy(const vector<Record>& data, int start,  int n, double x, vector<Order>& orderStatistics, ofstream& cashflowFile, ofstream& orderStatisticsFile, ofstream& finalPNLFile) {
+void basicStrategy(const vector<Record>& data, int start,  int n, int x, vector<Order>& orderStatistics, ofstream& cashflowFile, ofstream& orderStatisticsFile, ofstream& finalPNLFile) {
     double cashInHand = 0.0;
     int position = 0;
-
-    string prev = "wh";
+    n++;
 
     int inc = 1;
     int dec = 1;
 
-    for (size_t i = start - n + 1; i < start; ++i){
-        if (data[i].price - data[i - 1].price > 0) {
+    for (size_t i = start - n + 1; i < start; ++i) {
+        if (data[i].price > data[i - 1].price) {
             inc += 1;
             dec = 1;
         }
-        else if (data[i].price - data[i - 1].price < 0) {
+        else if (data[i].price < data[i - 1].price) {
             dec += 1;
             inc = 1;
         }
@@ -20,10 +19,6 @@ void basicStrategy(const vector<Record>& data, int start,  int n, double x, vect
 
     for (size_t i = start; i < data.size(); ++i) {
         double closePrice = data[i].price;
-
-        if (orderStatistics.size() != 0){
-            prev = orderStatistics.back().direction;
-        }
 
         if (data[i].price > data[i - 1].price) {
             inc += 1;
@@ -40,19 +35,25 @@ void basicStrategy(const vector<Record>& data, int start,  int n, double x, vect
             orderStatistics.push_back({date, "BUY", 1, closePrice});
             cashInHand -= closePrice;  
             position += 1;  
-        } else if (dec >= n && position > -x) {
+            orderStatisticsFile << orderStatistics.back().date << "," << orderStatistics.back().direction << "," << orderStatistics.back().quantity << "," << orderStatistics.back().price << "\n";
+        } 
+        else if (dec >= n && position > -x) {
             orderStatistics.push_back({date, "SELL", 1, closePrice});
             cashInHand += closePrice; 
             position -= 1;
-        }
-
-        cashflowFile << date << "," << cashInHand << "\n";
-
-        if ((orderStatistics.size() != 0) && (orderStatistics.back().direction != prev)) {
             orderStatisticsFile << orderStatistics.back().date << "," << orderStatistics.back().direction << "," << orderStatistics.back().quantity << "," << orderStatistics.back().price << "\n";
         }
 
+        // Write cash flow for the day
+        cashflowFile << date << "," << cashInHand << "\n";
     }
 
-    finalPNLFile << "Final PNL: " << cashInHand << "\n";
+    if (position != 0) {
+        double finalPrice = data.back().price;
+        double pnl = position * finalPrice;
+        pnl += cashInHand;
+        finalPNLFile << pnl;
+    } else {
+        finalPNLFile << 0.0;
+    }
 }
