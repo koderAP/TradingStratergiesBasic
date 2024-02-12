b0c82a45dc1d9c4f9595eb2bb066e143f911244d
ambarish1729 <77337286+ambarish1729@users.noreply.github.com>
2024-02-12 17:01:53 +0530

    Update RSI.h

--- RSI.h
+++ RSI.h
@@ -15,8 +15,6 @@ void RSIStrategy(const vector<Record>& data, int start, double x, int n, double 
     avgGain /= n;
     avgLoss /= n;
 
-    string prev = "wh";
-
     for (size_t i = start; i < data.size(); ++i) {
 
         double priceDiff = data[i].price - data[i - 1].price;
@@ -28,28 +26,30 @@ void RSIStrategy(const vector<Record>& data, int start, double x, int n, double 
         double RS = avgGain / avgLoss;
         double RSI = 100.0 - 100.0 / (1.0 + RS);
 
-        if (orderStatistics.size() != 0){
-            prev = orderStatistics.back().direction;
-        }
-
         string date = convert_to_date(data[i]);
 
         if (RSI < oversoldThreshold && position < x) {
             orderStatistics.push_back({date, "BUY", 1, data[i].price});
             cashInHand -= data[i].price;
             position += 1;
+            orderStatisticsFile << orderStatistics.back().date << "," << orderStatistics.back().direction << "," << orderStatistics.back().quantity << "," << orderStatistics.back().price << "\n";
         } else if (RSI > overboughtThreshold && position > -x) {
             orderStatistics.push_back({date, "SELL", 1, data[i].price});
             cashInHand += data[i].price;
             position -= 1;
+            orderStatisticsFile << orderStatistics.back().date << "," << orderStatistics.back().direction << "," << orderStatistics.back().quantity << "," << orderStatistics.back().price << "\n";
         }
         cashflowFile << date << "," << cashInHand << "\n";
-        if ((orderStatistics.size() != 0) && (orderStatistics.back().direction != prev)) {
-            orderStatisticsFile << orderStatistics.back().date << "," << orderStatistics.back().direction << "," << orderStatistics.back().quantity << "," << orderStatistics.back().price << "\n";
-        }   
 
     }
 
-    finalPNLFile << "Final PNL: " << cashInHand << "\n";
+    if (position != 0) {
+        double finalPrice = data.back().price;
+        double pnl = position * finalPrice;
+        pnl += cashInHand;
+        finalPNLFile << pnl;
+    } else {
+        finalPNLFile << 0.0;
+    }
 
 }
